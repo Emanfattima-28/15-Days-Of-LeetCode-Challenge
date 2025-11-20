7d580c6d3e2c4c541314eae3d14f466656557678
Eman Fatima <164496731+Emanfattima-28@users.noreply.github.com>
2025-11-20 18:58:20 +0500

    Time: 14 ms (97.29%) | Memory: 16.8 MB (40.24%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        groups = defaultdict(list)
+        for word in strs:
+            key = tuple(sorted(word))
+            groups[key].append(word)
+        return list(groups.values())
+        
